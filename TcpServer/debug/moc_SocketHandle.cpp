093f0d2b92711c9b73a19d48da31712f44d529c6
weizhao <Vstornzw@163.com>
2020-04-05 16:18:40 +0800

    regist

--- TcpServer/debug/moc_SocketHandle.cpp
+++ TcpServer/debug/moc_SocketHandle.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../SocketHandle.h"
+#include "../../../../../Git---DEV/CSgoujia/C-S-Host-Vdeo-Studio/C-S-Host-Vdeo-Studio/TcpServer/SocketHandle.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
